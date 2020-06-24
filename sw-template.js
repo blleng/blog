68e2c9f4b0df21be49d94df47f7ea2a30bbdc50d
blleng <l0bl@outlook.com>
2020-06-24 19:25:51 +0800

    PWA update

--- sw-template.js
+++ sw-template.js
@@ -3,7 +3,7 @@ const workboxVersion = '5.1.2';
 importScripts(`https://storage.googleapis.com/workbox-cdn/releases/${workboxVersion}/workbox-sw.js`);
 
 workbox.core.setCacheNameDetails({
-    prefix: "freezing"
+    prefix: "blleng"
 });
 
 workbox.core.skipWaiting();
