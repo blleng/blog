b888e407f5e50500c750624be203865c203e918e
blleng <l0bl@outlook.com>
2023-08-30 04:10:48 +0800

    stat on home page

--- src/helpers/userUtils.js
+++ src/helpers/userUtils.js
@@ -32,12 +32,12 @@ function getPositions(trees) {
 }
 
 const noteLabels = {
-  "note-icon-1": { label: "Sprouts", count: 0, icon: "note-icon-1" },
-  "note-icon-2": { label: "Saplings", count: 0, icon: "note-icon-2" },
-  "note-icon-3": { label: "Trees", count: 0, icon: "note-icon-3" },
-  "note-icon-4": { label: "Leaves", count: 0, icon: "note-icon-4" },
-  "note-icon-5": { label: "Guideboards", count: 0, icon: "note-icon-5" },
-  "note-icon-6": { label: "Droplets", count: 0, icon: "note-icon-6" },
+  "note-icon-1": { label: "Sprouts", count: 0, icon: "1" },
+  "note-icon-2": { label: "Saplings", count: 0, icon: "2" },
+  "note-icon-3": { label: "Trees", count: 0, icon: "3" },
+  "note-icon-4": { label: "Leaves", count: 0, icon: "4" },
+  "note-icon-5": { label: "Guideboards", count: 0, icon: "5" },
+  "note-icon-6": { label: "Droplets", count: 0, icon: "6" },
 };
 
 function forestData(data) {
